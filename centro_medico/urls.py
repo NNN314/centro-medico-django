282ae1fce5ce3334cd4bb757184ffe22a664521f
Nicolas Bosque <nicolasmartinbosque@gmail.com>
2025-05-02 03:45:29 -0300

    Configuración base lista

--- centro_medico/urls.py
+++ centro_medico/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from gestion_medica.views import home, about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('gestion_medica.urls')), 
+    path('', home, name="home"),
+    path('about/', about, name="about"),
 ]
+
+
 
